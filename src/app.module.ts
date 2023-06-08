8f3912fc8bb3ff0d1ab150e664572a292446f8c2
wassimbnh <104159727+wassimbnh@users.noreply.github.com>
2023-06-08 21:30:30 +0100

    connect to mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
-
+import { ConfigModule} from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true
+    }),
+    MongooseModule.forRoot(process.env.DB_URL),
+    UsersModule
+  ],
 
 })
 export class AppModule {}
